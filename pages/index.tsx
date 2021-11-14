17b4ffc8c34781d2f72ff623538079ee4c7c8c86
HojeongPark <parkhj062@gmail.com>
2021-11-14 18:26:13 +0900

    styled-components 설정 및 적용

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,13 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const Container = styled.div`
+  font-size: 30px;
+  color: green;
+`;
 
 const index: React.FC = () => {
-    return <div>hello world</div>
-}
+  return <Container>hello world</Container>;
+};
 
 export default index;
