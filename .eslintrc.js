0cc05f8cddaf71d888acb269c9feb5ed7b56d582
HojeongPark <parkhj062@gmail.com>
2021-11-14 17:24:35 +0900

    .eslintrc.js > add rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,16 @@ module.exports = {
         "@typescript-eslint"
     ],
     "rules": {
+        'arrow-parens': ['error', 'always'],
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off',
+        '@typescript-eslint/no-non-null-assertion': 'error',
+        '@typescript-eslint/no-unused-vars': 'warn',
+        '@typescript-eslint/no-inferrable-types': 'off',
+        '@typescript-eslint/ban-types': 'off',
+        '@typescript-eslint/no-empty-interface': 'warn',
+        '@typescript-eslint/no-empty-function': 'warn',
+        'react/prop-types': 'off',
+        'no-empty': 'off',
     }
 };
